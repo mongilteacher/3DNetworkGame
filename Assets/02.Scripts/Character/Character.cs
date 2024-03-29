f3277e699843423447bd5c062b038362112077dc
mongilteacher <mongilteacher@gmail.com>
2024-03-29 11:42:38 +0900

    죽고나서 5초후 리스폰

--- Assets/02.Scripts/Character/Character.cs
+++ Assets/02.Scripts/Character/Character.cs
@@ -96,8 +96,32 @@ public class Character : MonoBehaviour, IPunObservable, IDamaged
         
         GetComponent<Animator>().SetTrigger("Death");
         GetComponent<CharacterAttackAbility>().InactiveCollider();
+        
+        // 죽고나서 5초후 리스폰
+        if (PhotonView.IsMine)
+        {
+            StartCoroutine(Death_Coroutine());
+        }
     }
-    
+
+    private IEnumerator Death_Coroutine()
+    {
+        yield return new WaitForSeconds(5f);
+
+        PhotonView.RPC(nameof(Live), RpcTarget.All);
+        
+        transform.position = BattleScene.Instance.GetRandomSpawnPoint();
+    }
+
+    [PunRPC]
+    private void Live()
+    {
+        State = State.Live;
+
+        Stat.Init();
+        
+        GetComponent<Animator>().SetTrigger("Live");
+    }
     
     
     
