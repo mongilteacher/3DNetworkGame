086c1173e7c1bf860bf8bcad6ad64cbe92678ea6
mongilteacher <mongilteacher@gmail.com>
2024-04-02 16:41:49 +0900

    코드

---
+++ Assets/02.Scripts/UI/UI_PlayerRanking.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_PlayerRanking : MonoBehaviour
+{
+    public List<UI_PlayerRankingSlot> Slots;
+}
---
+++ Assets/02.Scripts/UI/UI_PlayerRankingSlot.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_PlayerRankingSlot : MonoBehaviour
+{
+    public Text RankingTextUI;
+    public Text NicknameTextUI;
+    public Text KillCountTextUI;
+    public Text ScoreTextUI;
+}
